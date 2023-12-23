bf311e5bcdc6e480910dc8922b48196072b9d082
Mritunjay <mritunjaygupta004@gmail.com>
2023-12-24 00:51:51 +0530

    Update thumbnail component

--- src/modules/products/components/thumbnail/index.tsx
+++ src/modules/products/components/thumbnail/index.tsx
@@ -52,13 +52,14 @@ const ImageOrPlaceholder = ({
       src={image}
       alt="Thumbnail"
       className="absolute inset-0 object-cover object-center"
-      draggable={false}
-      quality={50}
-      sizes="(max-width: 576px) 280px, (max-width: 768px) 360px, (max-width: 992px) 480px, 800px"
+      // draggable={false}
+      // quality={50}
+      // sizes="(max-width: 576px) 280px, (max-width: 768px) 360px, (max-width: 992px) 480px, 800px"
+
       fill
     />
   ) : (
-    <div className="w-full h-full absolute inset-0 flex items-center justify-center">
+    <div className="absolute inset-0 flex items-center justify-center w-full h-full">
       <PlaceholderImage size={size === "small" ? 16 : 24} />
     </div>
   )
